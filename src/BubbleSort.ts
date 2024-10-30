90f4eecb4b82877ddd114215fdb469d927054bc1
aadarsh-kawatra <thekawatra@gmail.com>
2024-10-30 18:19:52 +0530

    bubble sort optimized

--- src/BubbleSort.ts
+++ src/BubbleSort.ts
@@ -1,11 +1,17 @@
 export default function bubble_sort(arr: number[]): void {
-    for (let i = 0; i < arr.length - 1; i++) {
-        for (let j = 0; j < arr.length - 1 - i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                const temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+  const len = arr.length;
+  let swapped: boolean;
+
+  for (let i = 0; i < len - 1; i++) {
+    swapped = false;
+    for (let j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swapped = true;
+      }
     }
+    if (!swapped) break;
+  }
 }
